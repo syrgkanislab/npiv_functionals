14e7928a61d7ec862de1cd7ee95edba83eab6a5b
Vasileios Syrgkanis <vasy@microsoft.com>
2022-12-29 00:30:41 -0800

    made epsilon in moment parameterizable

--- mliv/neuralnet/moments.py
+++ mliv/neuralnet/moments.py
@@ -2,11 +2,15 @@ import torch
 import numpy as np
 
 # Returns the moment for the ATE example, for each sample in x
+
+
 def ate_moment_fn(x, test_fn, device):
     if torch.is_tensor(x):
         with torch.no_grad():
-            t1 = torch.cat([torch.ones((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
-            t0 = torch.cat([torch.zeros((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
+            t1 = torch.cat(
+                [torch.ones((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
+            t0 = torch.cat(
+                [torch.zeros((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
     else:
         t1 = np.hstack([np.ones((x.shape[0], 1)), x[:, 1:]])
         t0 = np.hstack([np.zeros((x.shape[0], 1)), x[:, 1:]])
@@ -17,8 +21,10 @@ def policy_moment_gen(policy):
     def policy_moment_fn(x, test_fn, device):
         with torch.no_grad():
             if torch.is_tensor(x):
-                t1 = torch.cat([torch.ones((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
-                t0 = torch.cat([torch.zeros((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
+                t1 = torch.cat(
+                    [torch.ones((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
+                t0 = torch.cat(
+                    [torch.zeros((x.shape[0], 1)).to(device), x[:, 1:]], dim=1)
             else:
                 t1 = np.hstack([np.ones((x.shape[0], 1)), x[:, 1:]])
                 t0 = np.hstack([np.zeros((x.shape[0], 1)), x[:, 1:]])
@@ -38,7 +44,7 @@ def trans_moment_gen(trans):
             if torch.is_tensor(x):
                 tx = torch.cat([x[:, [0]], trans(x[:, [1]]), x[:, 2:]], dim=1)
             else:
-                tx =  np.hstack([x[:, [0]], trans(x[:, [1]]), x[:, 2:]])
+                tx = np.hstack([x[:, [0]], trans(x[:, [1]]), x[:, 2:]])
         return test_fn(tx) - test_fn(x)
 
     return trans_moment_fn
@@ -50,15 +56,15 @@ def avg_der_moment_fn(x, test_fn, device):
         input = torch.cat([T, x[:, 1:]], dim=1)
         output = test_fn(input)
         gradients = torch.autograd.grad(outputs=output, inputs=T,
-                              grad_outputs=torch.ones(output.size()).to(device),
-                              create_graph=True, retain_graph=True, only_inputs=True)[0]
+                                        grad_outputs=torch.ones(
+                                            output.size()).to(device),
+                                        create_graph=True, retain_graph=True, only_inputs=True)[0]
     else:
         raise AttributeError('Not implemented')
     return gradients
 
-def avg_small_diff(x, test_fn, device):
-    epsilon = 0.01
 
+def avg_small_diff(x, test_fn, device, epsilon):
     if torch.is_tensor(x):
         with torch.no_grad():
             t1 = torch.cat([(x[:, [0]] + epsilon).to(device), x[:, 1:]], dim=1)
@@ -66,4 +72,4 @@ def avg_small_diff(x, test_fn, device):
     else:
         t1 = np.hstack([x[:, [0]] + epsilon, x[:, 1:]])
         t0 = np.hstack([x[:, [0]] - epsilon, x[:, 1:]])
-    return (test_fn(t1) - test_fn(t0)) / (2*epsilon)
+    return (test_fn(t1) - test_fn(t0)) / (2 * epsilon)
